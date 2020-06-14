1b2e2ba683f622533332a12b301e693a9db57ddb
Garvit Saxena <garvit.saxena@scool.in>
2020-06-14 21:25:40 +0530

    Updates

---
+++ precache-manifest.8dd5ea885aebb886c83baa4f00c1e278.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81fefdb3d7673bc39db38afe1bacaacd",
+    "url": "/Contact-CRUD-Firebase/index.html"
+  },
+  {
+    "revision": "f6a29fa0f3d235cf2bd1",
+    "url": "/Contact-CRUD-Firebase/static/css/main.52034aca.chunk.css"
+  },
+  {
+    "revision": "6991094ffb8f9ef039e1",
+    "url": "/Contact-CRUD-Firebase/static/js/2.a3e95f5c.chunk.js"
+  },
+  {
+    "revision": "003c96490c7aaee62db21e3b5f7ebec6",
+    "url": "/Contact-CRUD-Firebase/static/js/2.a3e95f5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6a29fa0f3d235cf2bd1",
+    "url": "/Contact-CRUD-Firebase/static/js/main.6008a91a.chunk.js"
+  },
+  {
+    "revision": "cfbd7c76eb0898023358",
+    "url": "/Contact-CRUD-Firebase/static/js/runtime-main.dc785f96.js"
+  }
+]);
